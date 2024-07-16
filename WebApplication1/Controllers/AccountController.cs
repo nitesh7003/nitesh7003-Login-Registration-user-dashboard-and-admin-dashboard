fa449031ed3c5cc31519eff0486f5c777b2c2e1f
nitesh7003 <yadavnitesh.1202@gmail.com>
2024-07-17 04:27:59 +0530

    Initial commit

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿// Controllers/AccountController.cs
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private static List<User> Users = new List<User>();
+        private const string AdminEmail = "admin@example.com";
+        private const string AdminPassword = "admin123";
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users.Add(model);
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model.Email == AdminEmail && model.Password == AdminPassword)
+            {
+                HttpContext.Session.SetString("UserEmail", model.Email);
+                HttpContext.Session.SetString("UserRole", "Admin");
+                return RedirectToAction("AdminDashboard", "Admin");
+            }
+            else if (Users.Any(u => u.Email == model.Email && u.Password == model.Password))
+            {
+                HttpContext.Session.SetString("UserEmail", model.Email);
+                HttpContext.Session.SetString("UserRole", "User");
+                return RedirectToAction("UserDashboard", "User");
+            }
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
